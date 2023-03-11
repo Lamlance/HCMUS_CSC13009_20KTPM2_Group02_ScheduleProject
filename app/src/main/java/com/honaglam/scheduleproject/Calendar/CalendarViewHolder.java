231311f8099b24073d7cb70890834793bd3c57a3
antrvan746 <86640630+antrvan746@users.noreply.github.com>
2023-03-11 18:52:49 +0700

    Minor changes

--- app/src/main/java/com/honaglam/scheduleproject/Calendar/CalendarViewHolder.java
+++ app/src/main/java/com/honaglam/scheduleproject/Calendar/CalendarViewHolder.java
@@ -9,18 +9,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.honaglam.scheduleproject.R;
 
-public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class CalendarViewHolder extends RecyclerView.ViewHolder {
   TextView txtDate;
   public CalendarViewHolder(@NonNull View itemView) {
     super(itemView);
-    itemView.setOnClickListener(this);
     txtDate = itemView.findViewById(R.id.txtCalendarDateItem);
-  }
-
-  @Override
-  public void onClick(View view) {
-    int position = getAdapterPosition();
-    // Toast.makeText(view.getContext(), "Da chon duoc ngay " + position, Toast.LENGTH_SHORT).show();
   }
 }
 
