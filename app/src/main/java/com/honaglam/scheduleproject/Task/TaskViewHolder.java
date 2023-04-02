ed3ac737866738f5ce1fad630f396b321b8607b0
Lamlance <lamhoangdien113@gmail.com>
2023-04-02 20:31:26 +0700

    Patches after merge

--- app/src/main/java/com/honaglam/scheduleproject/Task/TaskViewHolder.java
+++ app/src/main/java/com/honaglam/scheduleproject/Task/TaskViewHolder.java
@@ -1,52 +1,52 @@
 package com.honaglam.scheduleproject.Task;
 
 
-
 import android.graphics.Color;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.honaglam.scheduleproject.R;
+
 import kotlin.NotImplementedError;
 
 
-
 public class TaskViewHolder extends RecyclerView.ViewHolder {
-    
-    TextView txtTaskName;
-    TextView txtCountPomodoro;
-    CheckBox checkBoxCompleteTask;
-    ImageButton imgBtnDeleteTask;
+
+  TextView txtTaskName;
+  TextView txtCountPomodoro;
+  CheckBox checkBoxCompleteTask;
+  ImageButton imgBtnDeleteTask;
 
   OnClickPositionCallBack deleteTaskCallback = null;
 
   OnClickPositionCallBack checkTaskCallback = null;
-    ImageButton imgBtnEditTask;
+  ImageButton imgBtnEditTask;
 
 
   public interface OnClickPositionCallBack {
     void clickAtPosition(int position) throws NotImplementedError;
   }
 
-    public TaskViewHolder(@NonNull View itemView,
-                          OnClickPositionCallBack deleteTaskCallback,
-                          OnClickPositionCallBack checkTaskCallback,
-                          OnClickPositionCallBack editTaskCallback) {
-        super(itemView);
-        this.txtTaskName = itemView.findViewById(R.id.txtTaskName);
-        this.txtCountPomodoro = itemView.findViewById(R.id.txtCountPomodoro);
-        this.checkBoxCompleteTask = itemView.findViewById(R.id.checkBoxCompleteTask);
-        this.imgBtnDeleteTask = itemView.findViewById(R.id.imgBtnDeleteTask);
-        this.imgBtnEditTask = itemView.findViewById(R.id.imgBtnEditTask);
+  public TaskViewHolder(@NonNull View itemView,
+                        OnClickPositionCallBack deleteTaskCallback,
+                        OnClickPositionCallBack checkTaskCallback,
+                        OnClickPositionCallBack editTaskCallback) {
+    super(itemView);
+    this.txtTaskName = itemView.findViewById(R.id.txtTaskName);
+    this.txtCountPomodoro = itemView.findViewById(R.id.txtCountPomodoro);
+    this.checkBoxCompleteTask = itemView.findViewById(R.id.checkBoxCompleteTask);
+    this.imgBtnDeleteTask = itemView.findViewById(R.id.imgBtnDeleteTask);
+    this.imgBtnEditTask = itemView.findViewById(R.id.imgBtnEditTask);
 
     this.deleteTaskCallback = deleteTaskCallback;
-
     this.checkTaskCallback = checkTaskCallback;
 
-    itemView.setOnClickListener(new View.OnClickListener() {
+    imgBtnEditTask.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         try {
@@ -57,7 +57,6 @@ public class TaskViewHolder extends RecyclerView.ViewHolder {
         }
       }
     });
-
 
     this.imgBtnDeleteTask.setOnClickListener(new View.OnClickListener() {
       @Override
