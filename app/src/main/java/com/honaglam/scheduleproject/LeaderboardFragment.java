36b7d026d643185393366633730fad1d2d1c1101
pnkhang20 <pnkhang20@clc.fitus.edu.vn>
2023-05-09 17:15:40 +0700

    add expandable list

--- app/src/main/java/com/honaglam/scheduleproject/LeaderboardFragment.java
+++ app/src/main/java/com/honaglam/scheduleproject/LeaderboardFragment.java
@@ -8,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +26,7 @@ public class LeaderboardFragment extends Fragment {
 
     MainActivity mainActivity;
     Context context;
-
+    ExpandableListView expandableListView;
     public LeaderboardFragment() {
         // Required empty public constructor
     }
@@ -43,6 +47,13 @@ public class LeaderboardFragment extends Fragment {
         // Inflate the layout for this fragment
         mainActivity = (MainActivity) getActivity();
         context = requireContext();
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+
+        // Initialize ExpandableListView for leaderboard items
+        expandableListView = view.findViewById(R.id.recyclerLeaderboard);
+
         return inflater.inflate(R.layout.fragment_leaderboard, container, false);
     }
+
+
 }
